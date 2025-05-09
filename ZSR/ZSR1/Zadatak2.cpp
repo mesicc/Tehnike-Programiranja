9dc6d3b30fad1a0161b5c6771ff969547053af63
Kemal Mešić <153745485+mesicc@users.noreply.github.com>
2025-05-09 17:22:15 +0200

    Obrisao

--- ZSR/ZSR1/Zadatak2.cpp
+++
@@ -1,6 +0,0 @@
-/* Napišite program koji traži da se sa tastature unese brzina broda u čvorovima koja se zadaje
-isključivo kao cijeli broj (obavezno koristiti promjenljivu tipa “int”), a zatim izračunava i ispisuje
-brzinu broda u km/h kao decimalan broj. Koristite činjenicu da je čvor morska milja na sat, a da je
-jedna morska milja 1852 m (ovaj podatak obavezno definirati u programu kao konstantu). Na
-primjer, ukoliko se kao brzina broda unese broj 20, program treba da ispiše rezultat 37.04 jer je
-20 čvorova = 37.04 km/h. */
