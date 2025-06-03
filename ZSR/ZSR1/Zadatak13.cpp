a5809153bdbe751ba3606086aaf458a8c2b2f56a
Kemal Mešić <153745485+mesicc@users.noreply.github.com>
2025-06-03 10:07:52 +0200

    zsr1

--- ZSR/ZSR1/Zadatak13.cpp
+++ ZSR/ZSR1/Zadatak13.cpp
@@ -28,6 +28,5 @@ int main() {
         std::cout << "Aritmeticka sredina: " << aritmeticka << std::endl;
         std::cout << "Geometrijsak sredina: " << geometrijska << std::endl;
     }
-    
     return 0;
 }
