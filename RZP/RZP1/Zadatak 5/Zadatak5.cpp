fa49ce1f0f43d478a99b05ef143929804ec696e1
Kemal Mešić <153745485+mesicc@users.noreply.github.com>
2026-03-02 23:12:10 +0100

    RZP1 Zadatak5 V3

--- RZP/RZP1/Zadatak 5/Zadatak5.cpp
+++ RZP/RZP1/Zadatak 5/Zadatak5.cpp
@@ -5,19 +5,17 @@ int main() {
     std::cout << "Koliko zelite unijeti brojeva? ";
     std::cin >> n;
     std::cout << "Unesite brojeve:" << std::endl;
-
+    
     int najmanji_dosad, najveci_dosad;
     for(int i = 1; i <= n; i++) {
         int tekuci_broj;
         std::cout << i << ". broj: ";
         std::cin >> tekuci_broj;
-        
-        if(i == 1) najmanji_dosad = najveci_dosad = tekuci_broj;
-        else {
-            if(tekuci_broj < najmanji_dosad) najmanji_dosad = tekuci_broj;
-            if(tekuci_broj > najveci_dosad) najveci_dosad = tekuci_broj;
-        }
+
+        if(i == 1 || tekuci_broj < najmanji_dosad) najmanji_dosad = tekuci_broj;
+        if(i == 1 || tekuci_broj > najveci_dosad) najveci_dosad = tekuci_broj;
     }
+
     std::cout << "Najmanji medju unesenim brojevima je " << najmanji_dosad
               << ", a najveci medju unesenim brojevima je " << najveci_dosad << std::endl;
 
