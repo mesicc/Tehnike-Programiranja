3e1b2a9e22f75e795466d2e70b6d81d4ee4fa71a
Kemal Mešić <153745485+mesicc@users.noreply.github.com>
2026-03-04 15:58:20 +0100

    RZP1 Zadatak7 v2

--- RZP/RZP1/Zadatak 7/Zadatak7.cpp
+++ RZP/RZP1/Zadatak 7/Zadatak7.cpp
@@ -8,20 +8,15 @@ int main() {
 	for(int i = 1; i < visina; i++) std::cout << " ";
 	std::cout << "*\n";
 
-	int broj_razmaka1 = visina - 2;
-	int broj_razmaka2 = 1;
 
 	for(int red = 2; red < visina; red++) {
-		for(int i = 1; i <= broj_razmaka1; i++) std::cout << " ";
+		for(int i = 1; i <= visina - red; i++) std::cout << " ";
 		std::cout << "*";
-		for(int i = 1; i <= broj_razmaka2; i++) std::cout << " ";
+		for(int i = 1; i <= 2 * red - 3; i++) std::cout << " ";
 		std::cout << "*\n";
-
-		broj_razmaka1--; 
-		broj_razmaka2 += 2;
 	}
 
-	for(int i = 1; i <= broj_razmaka2 + 2; i++) std::cout << "*";
+	for(int i = 1; i < 2 * visina; i++) std::cout << "*";
  	std::cout << std::endl;
 	
 	return 0;
