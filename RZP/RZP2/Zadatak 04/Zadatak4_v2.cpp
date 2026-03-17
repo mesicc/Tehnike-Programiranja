47c98bedf5032911ecc5ed1b98ce98e9448ef9e8
Kemal Mešić <153745485+mesicc@users.noreply.github.com>
2026-03-17 02:46:42 +0100

    RZP2 Zadatak04

---
+++ RZP/RZP2/Zadatak 04/Zadatak4_v2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+typedef std::vector<int> Vektor;
+
+bool DaLiJeProst(int n) {
+    //Iz definicije je očigledno da nijedan cijeli broj manji od 2 nije prost, tako da iz funkcije odmah možemo vratiti logičku vrijednost “netačno” ukoliko je 𝑛 < 2. 
+    //svi parni brojevi osim 2 sigurno nisu prosti, tako da ako je 𝑛 paran broj različit od 2, možemo odmah reći da broj nije prost
+    if(n < 2 || n != 2 && n % 2 == 0) return false;
+    for(int i = 3; i <= n / i ; i++) 
+        if(n % i == 0) return false;
+    return true;
+}
+
+Vektor ProstiBrojeviUOpsegu(int a, int b) {
+    Vektor v;
+    for(int i = a; i <= b; i++) {
+        if(DaLiJeProst(i)) v.push_back(i);
+        if(i == b) break;                       // Ovo je zbog INT_MAX...
+    }
+    return v;
+}
+
+int main() {
+    int p, q;
+    std::cout << "Unesite pocetnu i krajnju vrijednost: ";
+    std::cin >> p >> q;
+    
+    auto v = ProstiBrojeviUOpsegu(p, q);
+    
+    if(v.size() == 0) std::cout << "Nema prostih brojeva u opsegu " << p << " i " << q << "!\n";
+    else {
+        std::cout << "Prosti brojevi u rasponu od " << p << " i " << q << " su: ";
+        for(int i = 0; i < v.size(); i++)
+            std::cout << v[i] << (i != v.size() - 1 ? ", " : "\n");
+    }
+    
+    return 0;
+}
